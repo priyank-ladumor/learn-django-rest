758ab0ee81bdee36eeaa03932e0c9c155b317f49
priyank-ladumor-2003 <priyank.suratinfotech@gmail.com>
2025-07-22 14:54:02 +0530

    manual students serializer add and set up student model for dynamic serializing

--- api/views.py
+++ api/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
-# Create your views here.
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status 
 
 def get_students(request):
-    students = {
-        'first_name': 'John',
-        'last_name': 'Doe'
-    }
-    return JsonResponse(students)
+    print('request: ', request)
+    students = Student.objects.all()
+    
+    # # manually convert to json or serializer
+    # student_list = list(students.values())
+    # print('student_list: ', student_list)
+    # # safe=False for non-dictionary data type to pass response
+    # return JsonResponse(student_list, safe=False)
+
+    serializer = StudentSerializer(students, many=True) # many=True for multiple objects get
+    # return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data)
--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    role_number = models.IntegerField(unique=True)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return f"{self.name}"
